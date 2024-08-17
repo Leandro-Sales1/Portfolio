2a541b38aa903b6d011a041bdf487c569961ec5f
Leandro Sales <lsl.2014@hotmail.com>
2024-08-17 10:50:22 -0300

    "Updated CurriculoSales.pdf, added/removed images, and modified CardAps.jsx component"

--- src/components/cardAps/CardAps.jsx
+++ src/components/cardAps/CardAps.jsx
@@ -1,5 +1,5 @@
 import { FaLinkedin, FaWhatsapp, FaAddressCard, FaGithub, FaReact, FaHtml5, FaBootstrap, FaFigma, FaNodeJs } from "react-icons/fa";
-import { SiTypescript, SiJavascript, SiTailwindcss, SiSass } from "react-icons/si"
+import { SiTypescript, SiJavascript, SiTailwindcss, SiSass, SiNextdotjs, SiJest, SiCypress, SiRedux,SiGraphql } from "react-icons/si"
 import { IoLogoCss3 } from "react-icons/io5";
 import { TbMailForward } from "react-icons/tb";
 
@@ -10,7 +10,7 @@ const CardAps = ({ corBotao, mostrarSecTitulo, mostrarSecTec }) => {
         <section className="flex flex-col items-center justify-center gap-4 md:gap-6 lg:gap-8">
             <div className={`m-4 mb-0 flex flex-col items-center justify-center ${mostrarSecTitulo}`}>
                 <div className="flex flex-col items-center justify-center gap-2 md:gap-4">
-                    <img className="w-40 h-40 rounded-full shadow-sm shadow-black  md:w-56 md:h-56" src="/imagens/fotoPrincipal.jpeg" alt="minha foto" />
+                    <img className="w-40 h-40 object-cover object-top rounded-full shadow-sm shadow-black  md:w-56 md:h-56" src="/imagens/fotoPrincipal.jpg" alt="minha foto" />
                     <h1 className="text-center text-3xl md:text-4xl">Leandro Sales</h1>
                     <h2 className="p-2 border border-black rounded-md text-sm shadow-sm shadow-black" style={{ backgroundColor: corBotao }}>Dev. Front-End.</h2>
                 </div>
@@ -28,7 +28,7 @@ const CardAps = ({ corBotao, mostrarSecTitulo, mostrarSecTec }) => {
             <div className={`m-4 mt-0 flex flex-col items-center justify-center gap-4 md:gap-6 lg:mt-12 lg:gap-8 ${mostrarSecTec ? '' : 'hidden'}`}>
                 <h2 className="text-center text-2xl md:text-3xl">Sobre mim:</h2>
                 <div className="flex flex-col items-center justify-center">
-                    <h3 className="text-lg w-[90%] text-justify md:text-xl lg:w-4/5 ">Sou amante de jogos e tecnologia desde a infância. Com o tempo, tenho me especializado cada vez mais na área de Engenharia de Software, focando atualmente em desenvolvimento front-end. Sendo um profissional bem-humorado, dedicado, organizado e adepto a mudanças, estando sempre em busca de novos desafios para aprimorar minhas habilidades.</h3>
+                    <h3 className="text-lg w-[90%] text-justify md:text-xl lg:w-4/5 ">Os jogos foram minha porta de entrada para o mundo da tecnologia na infância, despertando minha paixão pela área. Durante o curso de Engenharia de Software, identifiquei-me com o desenvolvimento. Sou um profissional bem-humorado, autodidata, dedicado, organizado, responsável, sempre em busca de novos desafios e adaptável às mudanças.</h3>
                 </div>
                 <h2 className="text-center text-2xl md:text-3xl">Tecnologias:</h2>
                 <div className="flex items-center justify-center gap-4 md:gap-6 lg:gap-8">
@@ -44,6 +44,13 @@ const CardAps = ({ corBotao, mostrarSecTitulo, mostrarSecTec }) => {
                     <SiSass className="icone" title="Sass" style={{ color: '#CD6799' }} />
                     <FaFigma className="icone" title="Figma" />
                     <FaNodeJs className="icone" title="NodeJS" style={{ color: '#3c873a' }} />
+                </div>
+                <div className="flex items-center justify-center gap-4 md:gap-6 lg:gap-8 ">
+                    <SiNextdotjs className="icone" title="Next.JS" style={{ color: "black" }} />
+                    <SiJest className="icone" title="Jest" style={{ color: '#CD6799' }} />
+                    <SiCypress className="icone" title="Cypress" style={{ color: "white" }}/>
+                    <SiRedux className="icone" title="Redux" style={{ color: '#9461fb' }}/>
+                    <SiGraphql className="icone" title="GraphQL" style={{ color: '#CD6799' }} />
                 </div>
             </div>
         </section>
