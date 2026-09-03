ec60b4f59062a3e7c686ee6d6361a2798ab4e96f
Leandro <lsl.2014@hotmail.com>
2026-09-03 13:21:12 -0300

    Update CV files to enhance content remove outdated footer image from the website.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,6 @@
 const Footer = () => {
   return (
-    <footer>
-      <div className="">
-        <img className="md:px-24 lg:p-0 lg:w-[50%] lg:ml-[18rem] 2xl:w-[45%] 2xl:ml-[24.5rem]" src="/imagens/rodape.png" alt="rodapé" />
-      </div>
+    <footer className="h-14">
     </footer>
   )
 }
